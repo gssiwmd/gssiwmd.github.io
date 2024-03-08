9cdfd4b3260c1f69f5ff1155df965ff2d11f862a
gssiwmd <gswangmd@qq.com>
2024-03-08 22:15:33 +0800

    update: 2024-03-08 22:15:33

--- update.sh
+++ update.sh
@@ -15,19 +15,13 @@ git clone  https://github.com/xiaorouji/openwrt-passwall-packages.git && cpdir o
 git clone  https://github.com/fw876/helloworld.git && cpdir helloworld
 
 # update istore
-git clone https://github.com/linkease/istore.git
-cp -rf istore/luci/* .
-rm -rf ./istore
+git clone https://github.com/linkease/istore.git && cp -rf istore/luci/* . ; rm -rf ./istore
+
 
 # update ddnsto etc.
-git clone https://github.com/linkease/nas-packages.git
-cp -rf ./nas-packages/multimedia/* .
-cp -rf ./nas-packages/network/services/* .
-rm -rf ./nas-packages
+git clone https://github.com/linkease/nas-packages.git && cp -rf ./nas-packages/multimedia/* . ; cp -rf ./nas-packages/network/services/* . ; rm -rf ./nas-packages
 
-git clone  https://github.com/linkease/nas-packages-luci.git
-cp -rf ./nas-packages-luci/luci/* .
-rm -rf ./nas-packages-luci
+git clone  https://github.com/linkease/nas-packages-luci.git && cp -rf ./nas-packages-luci/luci/* . ; rm -rf ./nas-packages-luci
 
 # push to github
 git add -A
