701f5b6f18111d16625ba749b42dd23757d530f0
gssiwmd <gswangmd@qq.com>
2024-03-08 14:55:31 +0800

    update: 2024-03-08 14:55:31

--- update.sh
+++ update.sh
@@ -6,28 +6,28 @@ rm -rf $1
 }
 
 # Update passwall
-git clone https://github.com/xiaorouji/openwrt-passwall passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall
 mvdir passwall
-git clone https://github.com/xiaorouji/openwrt-passwall-packages passwallpackages
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages passwallpackages
 mvdir passwallpackages
 
 # update helloworld
-git clone  https://github.com/fw876/helloworld.git
+git clone  --depth 1 https://github.com/fw876/helloworld.git
 rm -rf ./helloworld/LICENSE
 rm -rf ./helloworld/README.md
 mvdir helloworld
 
 # update istore
-git clone https://github.com/linkease/istore.git
+git clone --depth 1 https://github.com/linkease/istore.git
 mvdir istore/luci
 rm -rf ./istore
 
 # update ddnsto etc.
-git clone https://github.com/linkease/nas-packages nas
+git clone --depth 1 https://github.com/linkease/nas-packages nas
 mvdir nas/multimedia
 mvdir nas/network/services
 rm -rf nas
-git clone https://github.com/linkease/nas-packages-luci nasluci
+git clone --depth 1 https://github.com/linkease/nas-packages-luci nasluci
 mvdir nasluci/luci
 rm -rf nasluci
 
