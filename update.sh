f049700d3283e47f03e55da5c475d803c38f847e
gssiwmd <gswangmd@qq.com>
2024-03-08 16:10:51 +0800

    update: 2024-03-08 16:10:51

--- update.sh
+++ update.sh
@@ -35,7 +35,7 @@ cp -rf nas-packages/network/services/* .
 rm -rf nas-packages
 
 # git clone  https://github.com/linkease/nas-packages-luci.git
-# cp -rf nas-packages-luci/luci .
+# cp -rf nas-packages-luci/luci/* .
 # rm -rf ./nas-packages-luci
 
 # push to github
