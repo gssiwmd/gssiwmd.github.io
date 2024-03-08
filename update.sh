f245af6fe37b9e694d1daca3b8bbd6d0b98bcffc
gssiwmd <gswangmd@qq.com>
2024-03-08 16:13:34 +0800

    update: 2024-03-08 16:13:34

--- update.sh
+++ update.sh
@@ -30,12 +30,12 @@ rm -rf ./istore
 
 # update ddnsto etc.
 git clone https://github.com/linkease/nas-packages.git
-cp -rf nas-packages/multimedia/* .
-cp -rf nas-packages/network/services/* .
-rm -rf nas-packages
+cp -rf ./nas-packages/multimedia/* .
+cp -rf ./nas-packages/network/services/* .
+rm -rf ./nas-packages
 
 # git clone  https://github.com/linkease/nas-packages-luci.git
-# cp -rf nas-packages-luci/luci/* .
+# cp -rf ./nas-packages-luci/luci/* .
 # rm -rf ./nas-packages-luci
 
 # push to github
