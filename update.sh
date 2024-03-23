31bac26ecd3bc204c86f47a74f7775289615cead
gssiwmd <gswangmd@qq.com>
2024-03-23 08:05:06 +0800

    update: 2024-03-23 08:05:06

--- update.sh
+++ update.sh
@@ -5,17 +5,6 @@ cp -rf  `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
-# Update passwall and passwall2
-git clone  https://github.com/xiaorouji/openwrt-passwall.git && cpdir openwrt-passwall
-# git clone  https://github.com/gssiwmd/openwrt-passwall2.git && cpdir openwrt-passwall2
-git clone  https://github.com/xiaorouji/openwrt-passwall-packages.git 
-cp -rf ./openwrt-passwall-packages/brook ./
-cp -rf ./openwrt-passwall-packages/sing-box ./
-cp -rf ./openwrt-passwall-packages/trojan-go ./
-cp -rf ./openwrt-passwall-packages/trojan-plus ./
-cp -rf ./openwrt-passwall-packages/v2ray-geodata ./
-rm -rf ./openwrt-passwall-packages 
- 
 # update helloworld
 git clone  https://github.com/fw876/helloworld.git && cpdir helloworld
 
