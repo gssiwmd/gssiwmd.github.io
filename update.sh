4280baba4ae848c499f5c128375baa1c42c46fe2
gssiwmd <gswangmd@qq.com>
2024-03-31 08:49:42 +0800

    update: 2024-03-31 08:49:42

--- update.sh
+++ update.sh
@@ -5,6 +5,10 @@ cp -rf  `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
+#先删除除保留文件和目录以外的所有子目录及其文件、
+find * | grep -v '\<iptv\|README.md\|update.sh\|_config.yml\>' | xargs rm -rf 
+
+
 # update helloworld/passwall/passwall2
 # git clone  https://github.com/fw876/helloworld.git && cpdir helloworld
 git clone https://github.com/kenzok8/small.git && cpdir small
