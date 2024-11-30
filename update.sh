0b94576543e349b9ce32d50778e6406b56438794
gssiwmd <gssiwmd@gmail.com>
2024-11-30 06:05:43 +0000

    update: 2024-11-30 06:05:43

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ find * | grep -v '\<iptv\|README.md\|update.sh\|_config.yml\>' | xargs rm -rf
 
 # update passwall/passwall2 & openwrt-passwall-packages
 git clone https://github.com/xiaorouji/openwrt-passwall && cpdir openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2 && cpdir openwrt-passwall2
+# git clone https://github.com/xiaorouji/openwrt-passwall2 && cpdir openwrt-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages && cpdir openwrt-passwall-packages
 
 # update OpenClash
