43155362f13895f6d234d1ecc9fb54ec667804c6
gssiwmd <gssiwmd@gmail.com>
2026-01-19 06:20:11 +0000

    update: 2026-01-19 06:20:11

--- update.sh
+++ update.sh
@@ -18,6 +18,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages && cpdir openwr
  git clone -b dev --depth 1 https://github.com/vernesong/OpenClash  && cpdir OpenClash
 
 
+# update istore
+git clone https://github.com/linkease/istore.git && cp -rf istore/luci/* . ; rm -rf ./istore
+
 # update ddnsto etc.
 git clone https://github.com/linkease/nas-packages.git && cp -rf ./nas-packages/multimedia/* . ; cp -rf ./nas-packages/network/services/* . ; rm -rf ./nas-packages
 git clone  https://github.com/linkease/nas-packages-luci.git && cp -rf ./nas-packages-luci/luci/* . ; rm -rf ./nas-packages-luci
