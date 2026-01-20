9514636db773962cb211b83101a20f7377b408ac
gssiwmd <gssiwmd@gmail.com>
2026-01-20 12:00:35 +0000

    update: 2026-01-20 12:00:35

--- update.sh
+++ update.sh
@@ -22,8 +22,8 @@ git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-package
 # git clone https://github.com/linkease/istore.git && cp -rf istore/luci/* . ; rm -rf ./istore
 
 # update ddnsto etc.
-git clone https://github.com/linkease/nas-packages.git &&  cp -rf ./nas-packages/network/services/ddnsto . ; rm -rf ./nas-packages
-git clone  https://github.com/linkease/nas-packages-luci.git && cp -rf ./nas-packages-luci/luci/luci-app-ddnsto . ; rm -rf ./nas-packages-luci
+git clone --depth 1 https://github.com/linkease/nas-packages.git &&  cp -rf ./nas-packages/network/services/ddnsto . ; rm -rf ./nas-packages
+git clone --depth 1 https://github.com/linkease/nas-packages-luci.git && cp -rf ./nas-packages-luci/luci/luci-app-ddnsto . ; rm -rf ./nas-packages-luci
 
 # update argon theme
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git && rm -rf ./luci-theme-argon/.git; rm -rf  ./luci-theme-argon/.github
